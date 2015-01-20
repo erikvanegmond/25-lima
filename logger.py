2b4429cb076f9931759d0666843447c3f895da5e
Erik van Egmond <erikvane@gmail.com>
2015-01-20 15:56:12 +0100

    #7 added formatter to console logging

--- logger.py
+++ logger.py
@@ -2,5 +2,7 @@ import logging
 
 logging.basicConfig(level=logging.WARN, format='%(levelname)s [%(asctime)s] [%(filename)s:%(lineno)s] %(message)s', filename='logging.log',  filemode='w')
 console = logging.StreamHandler()
-console.setLevel(logging.DEBUG)
+console.setLevel(logging.INFO)
+formatter = logging.Formatter('%(levelname)s [%(asctime)s] [%(filename)s:%(lineno)s] %(message)s')
+console.setFormatter(formatter)
 logging.getLogger('').addHandler(console)
