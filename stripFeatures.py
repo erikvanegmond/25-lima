e76ee160e1ad1c4f51a884349d8f3db781a4dba7
sjmhoekstra <sebastiaan_hoekstra@hotmail.com>
2015-01-20 14:57:24 +0100

    #6 stripfeatures done

--- stripFeatures.py
+++ stripFeatures.py
@@ -4,8 +4,6 @@ import pprint as pp
 
 from functions import *
 
-
-
 def run():
     #assuming every folder has the same files in it to save time looking for files.
     directory = 'Fixed/Timelines-201408/20140801'
@@ -19,70 +17,55 @@ def run():
                 text = open(inputFile).read()
                 messageList = json.loads(text)
                 for i, message in enumerate(messageList):
-                    message = removeFeatures(message)
+                    message = extractFeatures(message)
                     messageList[i] = message
 
                 outDir = 'strippedFeatures/'+directory
                 if not os.path.exists(outDir):
                     os.makedirs(outDir)
                 with open(outDir+'/'+myFile, 'w') as f:
-                    f.write(json.dumps(messageList, indent=None))
+                    f.write(json.dumps(messageList, indent=1))
 
         except Exception as e:
             exc_type, exc_obj, exc_tb = sys.exc_info()
             print(exc_type)
             print inputFile
             print(exc_tb.tb_lineno)
-            print str(e)+"!"
+            print str(e)+"!"      
 
-def removeFeatures(message):
-    removeMessageFeaturesList = ["contributors",
-                                 "truncated",
-                                 "source",
-                                 "retweeted",
-                                 "in_reply_to_screen_name",
-                                 "id_str",
-                                 "favorited",
-                                 "geo",
-                                 "in_reply_to_user_id_str",
-                                 "possibly_sensitive",
-                                 "filter_level",
-                                 "in_reply_to_status_id_str",
-                                 "extended_entities",
-                                 "place"]
-    removeUserFeaturesList = ["profile_background_color",
-                              "profile_background_image_url_https",
-                              "default_profile",
-                              "default_profile_image",
-                              "follow_request_sent",
-                              "utc_offset",
-                              "profile_link_color",
-                              "profile_image_url",
-                              "following",
-                              "id_str",
-                              "geo_enabled",
-                              "profile_image_url_https",
-                              "profile_sidebar_border_color",
-                              "profile_sidebar_fill_color",
-                              "profile_text_color",
-                              "profile_use_background_image",
-                              "profile_banner_url",
-                              "profile_background_image_url",
-                              "profile_background_tile",
-                              "notifications",
-                              "contributors_enabled",
-                              "time_zone",
-                              "protected",
-                              "is_translator"]
-    for feature in removeMessageFeaturesList:
-        if feature in message:
+def extractFeatures(message):
+    relevantFeaturesList = ["text",
+                            "in_reply_to_status_id",
+                            "id",
+                            "favorite_count",
+                            "coordinates",
+                            "entities",
+                            "retweet_count",
+                            "in_reply_to_user_id",
+                            "user",
+                            "lang",
+                            "created_at"]
+    relevantUserFeaturesList = ["id",
+                            "verified",
+                            "followers_count",
+                            "listed_count",
+                            "statuses_count",
+                            "description",
+                            "friends_count",
+                            "location",
+                            "name",
+                            "lang",
+                            "favourites_count",
+                            "screen_name",
+                            "url",
+                            "created_at",
+                            "default_profile"]
+    for feature in message.keys():
+        if feature not in relevantFeaturesList:
             message.pop(feature, None)
-    if 'user' in message:
-        for feature in removeUserFeaturesList:
-            if feature in message['user']:
-                message['user'].pop(feature, None)
-    if "retweeted_status" in message:
-        message["retweeted_status"] = removeFeatures(message["retweeted_status"])
+    for userFeature in message['user'].keys():
+        if userFeature not in relevantFeaturesList:
+            message['user'].pop(userFeature, None)
 
     return message
 
