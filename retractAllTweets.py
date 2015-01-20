74e4d97ffdd71011e1989eba7bc83f90d94da89a
sjmhoekstra <sebastiaan_hoekstra@hotmail.com>
2015-01-20 15:39:59 +0100

    #6 nu ook deze verbeterd

--- retractAllTweets.py
+++ retractAllTweets.py
@@ -1,58 +1,56 @@
-import json
-import os, glob, sys
+import os, glob, sys, json
 
 from functions import *
 
-removeMessageFeaturesList = ["in_reply_to_status_id","id","favorite_count","coordinates","entities","retweet_count","in_reply_to_user_id","user","lang","timestamp_ms","retweeted_status","extended_entities"]
-
-#assuming every folder has the same files in it to save time looking for files.
-directory = 'Fixed/Timelines-201408/20140801'
-files = getFiles(directory)
-
-for i in range(1,32):
-    try:
-        directory = 'strippedFeatures/Fixed/Timelines-201408/201408%02d' % (i)
-        outDir = 'onlyText/'
-        if not os.path.exists(outDir):
-            os.makedirs(outDir)
-
-        counter = 0
-            
-        for myFile in files:
-            
-            inputFile = directory+'/'+myFile
-            text = open(inputFile).read()
-            messageList = json.loads(text)
-            for j, message in enumerate(messageList):
-                for feature in removeMessageFeaturesList:
-                    message.pop(feature, None)  
-                if len(message) > 2: 
-                    print message
-                    print len(message)
-                    counter += 1
-                    messageList.pop(j)
+def run():
+    #assuming every folder has the same files in it to save time looking for files.
+    directory = 'Fixed/Timelines-201408/20140801'
+    files = getFiles(directory)
     
-                    message.pop(feature, None)
-                if len(message) > 2:
-                    messageList.pop(j)
+    number_of_days = len([item for item in os.listdir('Fixed/Timelines-201408') if os.path.isdir(os.path.join('Fixed/Timelines-201408', item))])
+    
+    for i in range(1,number_of_days+1):
+        try:
+            directory = 'strippedFeatures/Fixed/Timelines-201408/201408%02d' % (i)
+            outDir = 'onlyText/'
+            if not os.path.exists(outDir):
+                os.makedirs(outDir)
+                
+            for myFile in files:
+                inputFile = directory+'/'+myFile
+                text = open(inputFile).read()
+                messageList = json.loads(text)
+                for j, message in enumerate(messageList):
+                    message = extractFeatures(message)
+                    messageList[j] = message  
 
-            if i == 1:
-                f = open(outDir+'/'+myFile, 'a')
-                f.write("[ ")
-            if messageList:
-                f = open(outDir+'/'+myFile, 'a')
-                f.write(json.dumps(messageList)[1:-1])
-                if i!=30:
+                if i == 1:
                     f = open(outDir+'/'+myFile, 'a')
-                    f.write(", ")
-                else:
+                    f.write("[ ")
+                if messageList:
                     f = open(outDir+'/'+myFile, 'a')
-                    f.write(" ]")
-        print counter        
+                    f.write(json.dumps(messageList, indent=1)[1:-1])
+                    if i!=number_of_days:
+                        f = open(outDir+'/'+myFile, 'a')
+                        f.write(", ")
+                    else:
+                        f = open(outDir+'/'+myFile, 'a')
+                        f.write(" ]")       
 
-    except Exception as e:
-            exc_type, exc_obj, exc_tb = sys.exc_info()
-            print(exc_type)
-            print inputFile
-            print(exc_tb.tb_lineno)
-            print str(e)+"!"
+        except Exception as e:
+                exc_type, exc_obj, exc_tb = sys.exc_info()
+                print(exc_type)
+                print inputFile
+                print(exc_tb.tb_lineno)
+                print str(e)+"!"
+                
+def extractFeatures(message):
+    relevantFeaturesList = ["text",
+                            "created_at"]
+    for feature in message.keys():
+        if feature not in relevantFeaturesList:
+            message.pop(feature, None)
+
+    return message
+
+run()                
