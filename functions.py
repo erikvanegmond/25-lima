296451dd4dfa887d229d6132b9ed191592b93219
Erik van Egmond <erikvane@gmail.com>
2015-01-21 17:09:52 +0100

    #13 actually add fixFiles and groupTweets to functions

--- functions.py
+++ functions.py
@@ -1,5 +1,5 @@
 from logger import *
-import os, glob
+import os, glob, sys
 import numpy as np
 import pandas as pd
 import re
@@ -32,6 +32,9 @@ def getFiles(directory = ".", fileFilter="*"):
     return files
 
 def getSitesFromFiles(fileList):
+    '''
+    Returns all the sites that have a file listed in the fileList
+    '''
     siteList = []
     for file in fileList:
         site = re.sub(r'(.csv)|(.json)|(HttpCheck-)|(TwitterNerStatus-)', '', file)
@@ -40,7 +43,6 @@ def getSitesFromFiles(fileList):
     return siteList
 
 def getDowntimeRanges(downtimeList, downtimeData):
-
     downtimeRanges = []
     for (i, downtimeMoment) in downtimeList.iteritems():
         #make sure we have some downtime data to get, if not just do it with minutes.
@@ -89,7 +91,6 @@ def getNgramsFromString(n, string):
         position+=1
     return ngrams
 
-
 def getNgramsFromMessageList(n, messageList):
     '''
     Return an Ngram dict if a list of messages is entered in this format:
@@ -128,4 +129,97 @@ def getNgramFrequenciesFromFiles(n, fileList):
     totalNr = len(ngrams)
     return (countDict, totalNr)
 
+def groupTweets(writeToFile=False):
+    #assuming every folder has the same files in it to save time looking for files.
+    directory = 'Fixed/Timelines-201408/20140801'
+    siteList = getSitesFromFiles(getFiles(directory))
+    # siteList = ['telegraaf.nl']
+    tweetFileTemplate = 'strippedFeatures/Fixed/Timelines-201408/201408%02d/%s.csv'
+    httpCheckFileTemplate = "HttpCheck/August 2014/201408%02d/HttpCheck-%s.json"
+
+    downtimeTweets = []
+    uptimeTweets = []
+
+    for site in siteList:
+        logging.info(site)
+        for i in range(1,32):
+            downtimeFile = httpCheckFileTemplate % (i, site)
+            tweetFile = tweetFileTemplate % (i, site)
+
+            logging.debug("201408%02d - %s" % (i, site))
+            try:
+                downtimeData = pd.read_json(downtimeFile)
+                tweetData = pd.read_json(tweetFile)
+            except:
+                logging.warn("%s or %s cannot be opened" % (downtimeFile, tweetFile))
+                continue
+
+            try:
+                if len(tweetData) and len(downtimeData):
+                    downtimeData['timestamp'] = pd.to_datetime(downtimeData['timestamp'], dayfirst=True)
+                    tweetData['created_at'] = pd.to_datetime(tweetData['created_at'], dayfirst=True)
+                    tweetData.index = pd.to_datetime(tweetData.pop('created_at'))
+                    downtimes = downtimeData[downtimeData["IsDown"].isin([True])]['timestamp']
+
+                    downtimeRanges = getDowntimeRanges(downtimes, downtimeData)
+                    uptimeRanges = getUptimeRanges(downtimeRanges, downtimeData)
+
+                    for downtimeRange in downtimeRanges:
+                        messageList = tweetDataToMessageList(tweetData[downtimeRange[0]:downtimeRange[1]])
+                        for message in messageList:
+                            if message not in uptimeTweets:
+                                downtimeTweets.append(message)
+
+                    for uptimeRange in uptimeRanges:
+                        messageList = tweetDataToMessageList(tweetData[uptimeRange[0]:uptimeRange[1]])
+                        for message in messageList:
+                            if message not in uptimeTweets:
+                                uptimeTweets.append(message)
+
+            except IndexError as e:
+                exc_type, exc_obj, exc_tb = sys.exc_info()
+                logging.error("IndexError on line %d,  %s" % (exc_tb.tb_lineno,e))
+
+            except Exception as e:
+                exc_type, exc_obj, exc_tb = sys.exc_info()
+                logging.error("Something went wrong at %d: %s, %s. Problematic files: %s or %s" % (exc_tb.tb_lineno,exc_type, e, downtimeFile, tweetFile))
+
+    if writeToFile:
+        with open("downtimeTweets.json", 'w') as f:
+            f.write(json.dumps(downtimeTweets, indent=1))
+
+        with open("uptimeTweets.json", 'w') as f:
+            f.write(json.dumps(uptimeTweets, indent=1))
+
+    return (downtimeTweets, uptimeTweets)
+
+def fixFiles(directoryTemplate):
+    for i in range(1,32):
+        try:
+            directory = directoryTemplate % (i)
+            files = []
+            try:
+                os.chdir(directory)
+                for file in glob.glob("*.csv"):
+                    files.append(file)
+                os.chdir('../..')
+
+
+                for myFile in files:
+                    inputFile = directory+'/'+myFile
+                    text = open(inputFile).read()
+                    text = re.sub("\x00", "", text)
+
+                    outDir = 'Fixed/'+directory
+                    if not os.path.exists(outDir):
+                        os.makedirs(outDir)
+                    with open(outDir+'/'+myFile, 'w') as outfile:
+                        outfile.write(text)
+
+            except Exception as e:
+                exc_type, exc_obj, exc_tb = sys.exc_info()
+                logging.warn("Something went wrong at:%d, %s" %( exc_tb.tb_lineno, str(e)))
+                break
+        except:
+            continue
 
